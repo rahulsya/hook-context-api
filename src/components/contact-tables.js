811fc9054833995fcf949c9508ca5d73cff23a69
rahulsya <rahulsyaban@gmail.com>
2020-09-30 15:51:41 +0700

    complete using useReducer

--- src/components/contact-tables.js
+++ src/components/contact-tables.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { Segment, Table, Button, Icon } from "semantic-ui-react";
+import { ContactContex } from "../context/contact-context";
 
-export default function contact-tables() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function ContactTable() {
+  const { state, dispatch } = React.useContext(ContactContex);
+  console.log(state);
+  // local state unutk komponen internal
+  const [selectedId, setSelectedId] = React.useState();
+
+  // dispatch data ke context
+  const delContact = (id) => {
+    dispatch({
+      type: "DEL_CONTACT",
+      payload: id,
+    });
+  };
+
+  const onRemoveUser = () => {
+    delContact(selectedId);
+    setSelectedId(null);
+  };
+
+  const rows = state.contacts.map((contact) => (
+    <Table.Row
+      key={contact.id}
+      onClick={() => setSelectedId(contact.id)}
+      active={contact.id === selectedId}
+    >
+      <Table.Cell>{contact.id}</Table.Cell>
+      <Table.Cell>{contact.name}</Table.Cell>
+      <Table.Cell>{contact.email}</Table.Cell>
+    </Table.Row>
+  ));
+
+  return (
+    <Segment>
+      <Table called striped selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{rows}</Table.Body>
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell colSpan="4">
+              <Button
+                floated="right"
+                icon
+                labelPosition="left"
+                color="red"
+                size="small"
+                disabled={!selectedId}
+                onClick={onRemoveUser}
+              >
+                <Icon name="trash" /> Remove User
+              </Button>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </Segment>
+  );
 }
