326a2a736858490dfff98efb9d2f57e592c8ac94
rahulsya <rahulsyaban@gmail.com>
2020-09-30 15:52:57 +0700

    modified app js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 
 import CounterView from "./views/counter-view";
+import ContactView from "./views/contact-view";
 
 function App() {
   return (
     <Container>
       <h1>hooks context</h1>
-      <CounterView />
+      {/* <CounterView /> */}
+      <ContactView />
     </Container>
   );
 }
