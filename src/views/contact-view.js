edad6c46827710c9862f390f39cdc4dd927b4511
rahulsya <rahulsyaban@gmail.com>
2020-09-30 15:52:05 +0700

    complete using useReducer

--- src/views/contact-view.js
+++ src/views/contact-view.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Segment, Header } from "semantic-ui-react";
 import { ContactContextProvider } from "../context/contact-context";
 
+import ContactTable from "../components/contact-tables";
+import ContactForm from "../components/contact-form";
+
 export default function ContactView() {
   return (
     <ContactContextProvider>
       <Segment basic>
         <Header as="h3">Contacts</Header>
+        <ContactForm />
+        <ContactTable />
       </Segment>
     </ContactContextProvider>
   );
