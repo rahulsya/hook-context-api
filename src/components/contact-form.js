152b6c28668aac76c335f66d47f8d29205ff2fac
rahulsya <rahulsyaban@gmail.com>
2020-09-30 15:51:34 +0700

    complete using useReducer

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,5 +1,69 @@
 import React from "react";
+import { Segment, Form, Input, Button } from "semantic-ui-react";
+import _ from "lodash";
+import { ContactContex } from "../context/contact-context";
 
 export default function ContactForm() {
-  return <div></div>;
+  const name = useFormInput("");
+  const email = useFormInput("");
+
+  console.log(name);
+  // akses ke context
+  let { state, dispatch } = React.useContext(ContactContex);
+
+  const onSubmit = () => {
+    // dispatch({
+    //   type: "START",
+    // });
+
+    dispatch({
+      type: "ADD_CONTACT",
+      payload: { id: _.uniqueId(10), name: name.value, email: email.value },
+    });
+
+    // dispatch({
+    //   type: "COMPLETE",
+    // });
+    // reset form
+    name.onReset();
+    email.onReset();
+  };
+
+  return (
+    <Segment basic>
+      <Form onSubmit={onSubmit}>
+        <Form.Group widths={3}>
+          <Form.Field width={6}>
+            <Input placeholder="enter name" {...name} required />
+          </Form.Field>
+          <Form.Field width={6}>
+            <Input placeholder="enter email" {...email} required />
+          </Form.Field>
+
+          <Form.Field width={6}>
+            <Button fluid primary disabled={state.loading}>
+              New contact
+            </Button>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+    </Segment>
+  );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = React.useState(initialValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleReset = () => {
+    setValue("");
+  };
+  return {
+    value,
+    onChange: handleChange,
+    onReset: handleReset,
+  };
 }
